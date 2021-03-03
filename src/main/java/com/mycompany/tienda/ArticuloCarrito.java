725235c7a86cf3e816b1a0a277819daf586cd34f
Nayra Deniz <nayradb@gmail.com>
2021-03-03 10:47:35 +0100

    Version 1.0 con Netbeans

---
+++ src/main/java/com/mycompany/tienda/ArticuloCarrito.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.tienda;
+/**
+ * @author Nayra
+ *
+ */
+public class ArticuloCarrito {
+	Articulo elemento;
+	int cantidad;
+	
+	public ArticuloCarrito() {};
+	
+	public ArticuloCarrito(Articulo articulo, int cantidad) {
+		this.elemento = articulo;
+		this.cantidad = cantidad;
+	}
+	
+	@Override
+	public String toString() {
+		return this.elemento + "x" + this.cantidad + "\n";
+	}
+}
+
